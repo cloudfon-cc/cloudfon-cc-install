125466f3d00b936205bbf7d1b8a2ab38e8e43190
cloudfon_cc <114638543+cloudfon-cc@users.noreply.github.com>
2022-10-19 09:39:27 +0800

    Update cloudfon_cc_ctl.sh

--- v1.1.0.x/cloudfon_cc_ctl.sh
+++ v1.1.0.x/cloudfon_cc_ctl.sh
@@ -17,7 +17,7 @@ version: '3.9'
 services:
   # callcenter api
   cc_api:
-    image: puteyun/cloud_contact_center:0.1.5
+    image: puteyun/cloud_contact_center:0.1.6
     container_name: cc_api
     volumes:
       - ./:/data
