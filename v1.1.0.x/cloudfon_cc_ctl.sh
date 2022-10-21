aecbad9a28327ff6577e73187059b637f6043514
cloudfon_cc <114638543+cloudfon-cc@users.noreply.github.com>
2022-10-21 10:50:12 +0800

    Update cloudfon_cc_ctl.sh

--- v1.1.0.x/cloudfon_cc_ctl.sh
+++ v1.1.0.x/cloudfon_cc_ctl.sh
@@ -17,7 +17,7 @@ version: '3.9'
 services:
   # callcenter api
   cc_api:
-    image: puteyun/cloud_contact_center:0.1.6
+    image: puteyun/cloud_contact_center:0.1.7
     container_name: cc_api
     extra_hosts:
       - host.docker.internal:host-gateway
