d56740713474bf8a9201d15edd05d162526ce911
cloudfon_cc <114638543+cloudfon-cc@users.noreply.github.com>
2023-02-28 13:06:00 +0800

    Update cloudfon_cc_ctl.sh

--- v2.0.0.x/cloudfon_cc_ctl.sh
+++ v2.0.0.x/cloudfon_cc_ctl.sh
@@ -101,7 +101,7 @@ create() {
     shift
 
     # 获取镜像名称
-    image='puteyun/cloud_contact_center:2.0.4'
+    image='puteyun/cloud_contact_center:2.0.5'
     
     while getopts 'i:' opt; do
         case "${opt}" in
